76db82f2685556888e81c902c97879eda862c761
edho0219 <edho0219@gmail.com>
2023-06-12 08:03:17 +0700

    initial commit

---
+++ src/latihan1/halo.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan1;
+
+/**
+ *
+ * @author USER
+ */
+public class halo {
+    
+    public static void main(String[] args) {
+        String nama = "Muhammad Ridho";
+        int npm = 2110010148;
+        String kelas = "4D Reguler BJB Pagi";
+        String jurusan = "Teknik Informatika";
+        String ttl  = "Barikin, 19 Februari 2003";
+        
+        System.out.println("==========================");
+        System.out.println("\t BIODATA");
+        System.out.println("==========================");
+        System.out.println("Nama \t\t\t : "+nama);
+        System.out.println("NPM \t\t\t : "+npm);
+        System.out.println("Kelas \t\t\t : "+kelas);
+        System.out.println("Jurusan \t\t : "+jurusan);
+        System.out.println("Tempat Tanggal Lahir \t : "+ttl);
+        System.out.println("==========================");
+    }
+}
